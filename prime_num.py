b5145eb88ea97742be567da3fb0f17a7510d6fa6
karthi0079 <146184103+karthi0079@users.noreply.github.com>
2025-05-08 21:12:04 +0530

    Update prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -6,7 +6,6 @@ def is_prime(num):
             return False
     return True
 
-# Example usage:
 number = int(input("Enter a number: "))
 if is_prime(number):
     print(f"{number} is a prime number.")
